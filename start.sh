d724b9bac513c630b97ce9777cc74c105f1cd010
Bikash Maity <110729541+PROFESSOR-9@users.noreply.github.com>
2022-08-18 00:21:07 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/PROFESSOR-9/TonyStark-Robot.git /TonyStark-Robot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /TonyStark-Robot
 fi
-cd /EvaMaria
+cd /TonyStark-Robot
 pip3 install -U -r requirements.txt
-echo "Starting Bot...."
+echo "Starting TonyStark Robot...."
 python3 bot.py
