d7e4a64595ff6b117bb050fdd55f9f25f3784853
Bikash Maity <110729541+PROFESSOR-9@users.noreply.github.com>
2022-08-25 11:03:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ logging.getLogger("imdbpy").setLevel(logging.ERROR)
 
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
-from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR
+from info import SESSION, API_ID, API_HASH, BOT_TOKEN, REQUEST_MESSAGE
 from utils import temp
 from database.ia_filterdb import Media
 from database.users_chats_db import db
